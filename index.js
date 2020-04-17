d2eb26218e7330ec1e1c256a1946a0980a4be777
Zack Harley <zackharley@gmail.com>
2020-04-17 19:09:56 -0400

    temp

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const github = require('@actions/github');
 async function run() {
   const myToken = core.getInput('githubToken');
   const octokit = new github.GitHub(myToken);
-  const { data } = await octokit.pulls.get({
-    owner: 'pickleheads',
-    repo: 'cookbook',
+  const owner = 'pickleheads';
+  const repo = 'cookbook';
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner,
+    repo,
     pull_number: 9,
   });
-  // const response = await octokit.checks.listForRef({
-  //   owner,
-  //   repo,
-  //   ref,
-  // });
-  console.log({ data: JSON.stringify(data, null, 2) });
+  const ref = pullRequest.head.ref;
+  const { response } = await octokit.checks.listForRef({
+    owner,
+    repo,
+    ref,
+  });
+  console.log({ data: JSON.stringify(response, null, 2) });
 }
 
 run().catch((error) => core.setFailed(error.message));
