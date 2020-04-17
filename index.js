ba5829c18b635b9f52a80c3d0f505607fc134d83
Zack Harley <zackharley@gmail.com>
2020-04-17 19:49:39 -0400

    temp

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const github = require('@actions/github');
 async function run() {
   const myToken = core.getInput('githubToken');
   const octokit = new github.GitHub(myToken);
-  console.log(core.getInput('githubRepo'));
-  const owner = 'pickleheads';
-  const repo = 'cookbook';
-  const { data: pullRequest } = await octokit.pulls.get({
-    owner,
-    repo,
-    pull_number: 9,
-  });
-  const ref = pullRequest.head.ref;
-  const { data: pullRequestChecks } = await octokit.checks.listForRef({
-    owner,
-    repo,
-    ref,
-  });
-  const wereChecksSuccessful = _.every(pullRequestChecks.check_runs, {
-    conclusion: 'status',
-  });
-  const shouldPostGif = wereChecksSuccessful;
-  console.log({ shouldPostGif });
+  console.log(github.context);
+  // const repo = github.context.payload.repository;
+  // const pullRequestNumber = github.context.payload.pull_request.number;
+  // const { data: pullRequest } = await octokit.pulls.get({
+  //   owner,
+  //   repo,
+  //   pull_number: pullRequestNumber,
+  // });
+  // const ref = pullRequest.head.ref;
+  // const { data: pullRequestChecks } = await octokit.checks.listForRef({
+  //   owner,
+  //   repo,
+  //   ref,
+  // });
+  // const wereChecksSuccessful = _.every(pullRequestChecks.check_runs, {
+  //   conclusion: 'status',
+  // });
+  // const shouldPostGif = wereChecksSuccessful;
+  // console.log({ shouldPostGif });
 }
 
 run().catch((error) => core.setFailed(error.message));
