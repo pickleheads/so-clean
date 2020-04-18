662e0220e637bc9775987d9a4a9eacc45c8f29ce
Zack Harley <zackharley@gmail.com>
2020-04-18 09:39:29 -0400

    temp

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const _ = require('lodash');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const SO_CLEAN_IMAGE_URL =
+  'https://lh3.googleusercontent.com/0Ca6E4Q-NwkhwwRIJWf2g4C9bUd7HmgvIgwzcREbiqkQwf0NNXA2uoxYeQs1W3s9AclXyVoniHsp0a7bZoU0-Uf1U9Rxy1mI1lzY2wzg7TSZobyeOaLm4IF-o_TAIg_pDs3RqbnmvkjbabpS7cKEoPqOeWtPFuD8aBF7iZXzgqMFkI6kPlYqwY9qqI8cGi13VFFMICIMfQSt1Bz7TLb7poc5xfKNyb07auCEMpikRamaxv_ucVEoggmaftmMiOrC2vJcCTRSfwmyE2D-BTEOp2wqSts37Im4XddIQx2_nyuq1SV0oEt4lXb4rtxU9d3mSi5xCUneeqc5TBUPfE9Ovq4KP8YyeXvZ0brJv3aKW5pDwlkXWrciZm1pqXpOtO5uOyrYQEITzKCL0BDsjqzxljKp5UTEpvDB4wYz9RgB8sUeSTvwjtKCIHtw9tazn820MCMKSGLKpLom6KTYAdrGu0ljfpYZ-pS8dXOex1XcptUclntiWrY2GeFV099-O-KTVhy0NSEmfNUbDuzpmSrj89gg_044lmTxLAiciHDRkuLrjNq1aMMnvkBqz8QVmuOKcyNZ4G4O1nGBaku_njo9ksJdWsikUZ-m9BC6KHcMah_2A4tBvLGRaNrDI1XaFED4AqmiLoy0RQ342NE_r7yX9mqUQtYFKed1qcibVFLaCcWIy35V5TkAJN0KQMFuMNeCcTCdogThhyKc2uyTwKqP8wt1T2jQG2d0cFAMxF9YTOiHs0zAHJ4rRg=w550-h366-no';
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
 
-async function run() {
+async function action() {
   const myToken = core.getInput('githubToken');
   const octokit = new github.GitHub(myToken);
   const owner = github.context.payload.organization.login;
   const repo = github.context.payload.repository.name;
   const pullRequestNumber = github.context.payload.pull_request.number;
+  // TODO: Remove previous comment
+  const mergeable = await fetchMergeableStatus(octokit, owner, repo);
+  if (mergeable) {
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: pullRequestNumber,
+      body: `![So clean](${SO_CLEAN_IMAGE_URL})`,
+    });
+  }
+}
+
+async function fetchMergeableStatus(octokit, owner, repo, pullRequestNumber) {
   let meregable = null;
   do {
     await sleep(500);
@@ -19,18 +35,9 @@ async function run() {
       pull_number: pullRequestNumber,
     });
     meregable = pullRequest.mergeable;
-    console.log({ meregable });
+    core.debug({ meregable });
   } while (meregable === null);
-  // const ref = pullRequest.head.ref;
-  // const { data: pullRequestChecks } = await octokit.checks.listForRef({
-  //   owner,
-  //   repo,
-  //   ref,
-  // });
-  // const wereChecksSuccessful = _.every(pullRequestChecks.check_runs, {
-  //   conclusion: 'status',
-  // });
-  // const shouldPostGif = wereChecksSuccessful
+  return meregable;
 }
 
-run().catch((error) => core.setFailed(error.message));
+action().catch((error) => core.setFailed(error.message));
