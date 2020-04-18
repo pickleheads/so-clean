401c603eea23e2643f27937de2be5701cf1eef33
Zack Harley <zackharley@gmail.com>
2020-04-18 09:04:44 -0400

    temp

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ const _ = require('lodash');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+
 async function run() {
   const myToken = core.getInput('githubToken');
   const octokit = new github.GitHub(myToken);
   const owner = github.context.payload.organization.login;
   const repo = github.context.payload.repository.name;
   const pullRequestNumber = github.context.payload.pull_request.number;
-  const { data: pullRequest } = await octokit.pulls.get({
-    owner,
-    repo,
-    pull_number: pullRequestNumber,
-  });
-  const ref = pullRequest.head.ref;
-  const { data: pullRequestChecks } = await octokit.checks.listForRef({
-    owner,
-    repo,
-    ref,
-  });
-  const wereChecksSuccessful = _.every(pullRequestChecks.check_runs, {
-    conclusion: 'status',
-  });
-  const shouldPostGif = wereChecksSuccessful;
-  console.log(JSON.stringify({ pullRequest: github.context.payload.pull_request, shouldPostGif }, null, 2));
+  let meregable = null;
+  do {
+    await sleep(500);
+    const { data: pullRequest } = await octokit.pulls.get({
+      owner,
+      repo,
+      pull_number: pullRequestNumber,
+    });
+    meregable = pullRequest.mergeable;
+    console.log({ meregable });
+  } while (meregable === null);
+  // const ref = pullRequest.head.ref;
+  // const { data: pullRequestChecks } = await octokit.checks.listForRef({
+  //   owner,
+  //   repo,
+  //   ref,
+  // });
+  // const wereChecksSuccessful = _.every(pullRequestChecks.check_runs, {
+  //   conclusion: 'status',
+  // });
+  // const shouldPostGif = wereChecksSuccessful
 }
 
 run().catch((error) => core.setFailed(error.message));
